ca6fe8aa770a082582a63f4fe293938810836202
vapanov <valentin.panov@me.com>
2020-10-15 00:04:55 +0300

    1.3-1 1st edition

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,34 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
     // код для задачи №1 писать здесь
-    // return totalAmount;
+    "use strict";
+    debugger;
+    let errorMsg;
+
+    if (isNaN(percent) || !percent) errorMsg = `Параметр "процент" содержит неправильное значение ${percent}.`;
+    if (isNaN(contribution)) errorMsg = `Параметр "сумма первоначального взноса" содержит неправильное значение ${contribution}.`;
+    if (isNaN(amount) || !amount) errorMsg = `Параметр "сумма кредита" содержит неправильное значение ${amount}.`;
+    if (isNaN(date) || !date) errorMsg = `Параметр "срок погашения кредита" содержит неправильное значение ${date}.`;
+
+    if (errorMsg) {
+      console.log(errorMsg);
+      return errorMsg;
+    }
+
+    // тело кредита
+    let totalPrincipal = amount - contribution;
+
+    // количество выплачиваемых месяцев вводится в функцию параметром date, если верить легенде дз
+    let now = new Date();
+    let month = date.getMonth() - now.getMonth() + (12 * (date.getFullYear() - now.getFullYear()));
+
+    let monthPercent = percent / (12 * 100);
+
+    let monthPayment = totalPrincipal * (monthPercent + monthPercent / (((1 + monthPercent) ^ month) - 1));
+
+    let totalAmount = (monthPayment * month).toFixed(2);
+
+    console.log(totalAmount);
+    return totalAmount;
 }
 
 function getGreeting(name) {
