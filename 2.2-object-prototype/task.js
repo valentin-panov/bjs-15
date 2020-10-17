fc4c50d8097a939e968baecb4b284e57e0d4d60f
vapanov <valentin.panov@me.com>
2020-10-17 22:33:17 +0300

    2.2-1 j-g

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,9 @@
-//String.prototype.isPalindrome - для задачи №1
+// TASK 1
+String.prototype.isPalindrome = function isPalindrome() {
+   'use strict';
+   if (this.toUpperCase().replace(/[\s.,%]/g, '') === this.toUpperCase().replace(/[\s.,%]/g, '').split("").reverse().join("")) return true;
+   return false;
+}
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
