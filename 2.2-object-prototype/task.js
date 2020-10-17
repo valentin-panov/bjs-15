ace11c1f084e0a542ccaf297b707a43b8db6b0a8
vapanov <valentin.panov@me.com>
2020-10-17 22:48:03 +0300

    2.2[1+2] j-g

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,13 +1,23 @@
 // TASK 1
-String.prototype.isPalindrome = function isPalindrome() {
+String.prototype.isPalindrome = function() { // не уверен, нужно ли давать имя этой функции, на лекции всё было так скомкано %( а жасмин зелёный и так.
    'use strict';
    if (this.toUpperCase().replace(/[\s.,%]/g, '') === this.toUpperCase().replace(/[\s.,%]/g, '').split("").reverse().join("")) return true;
    return false;
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+   if (!marks) return 0;
+
+   let sumMarks = 0;
+   let countMarks = 0;
+
+   for (let key in marks) {
+      sumMarks += marks[key];
+      countMarks++;
+   }
+   if (countMarks == 0) return 0;
+
+   return Math.round(sumMarks / countMarks);
 }
 
 function checkBirthday(birthday) {
