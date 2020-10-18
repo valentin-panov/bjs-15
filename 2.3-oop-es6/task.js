15a7c6231000083d2b7a964035ec6ace377d5fd5
vapanov <valentin.panov@me.com>
2020-10-18 13:47:33 +0300

    2.3 all green

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -99,3 +99,44 @@ class Library {
 
 // TASK 3
 
+class StudentLog {
+   constructor (name) {
+      this.name = name;
+   }
+
+   getName() {
+      return this.name;
+   }
+
+   addGrade(grade, subject) {
+      if (!subject) return "empty subject input";
+      if (!(subject in this)) this[subject] = [];
+      if (grade === 1,2,3,4,5) {
+         this[subject].push(grade);
+         return this[subject].length;
+      }
+      return `Вы пытались поставить оценку "${grade}" по предмету "${subject}". Допускаются только числа от 1 до 5.\n${this[subject].length}`;
+   }
+
+   getAverageBySubject(subject) {
+      if (!subject) return "empty subject input";
+      if (!(subject in this)) return 0;
+      let sumMarks = 0;
+      for (let i = 0; i < this[subject].length; i++) {
+         sumMarks += this[subject][i];
+      }
+      if (typeof sumMarks == "number" && sumMarks != 0) return sumMarks/this[subject].length;
+      return 0;
+   }
+
+   getTotalAverage() {
+      debugger;
+      let sumMarks = 0, keysCount = 0;
+      for (let key in this) {
+         sumMarks += this.getAverageBySubject(key);
+         if (key != "name") keysCount++; // не нашёл, как проверять количество ключей в объекте ( исключение только для имени, если объект будет усложняться - нужно будет дорабатывать проверку.
+      }
+      if (typeof sumMarks == "number" && sumMarks != 0) return sumMarks/keysCount;
+      return 0;
+   }
+}
