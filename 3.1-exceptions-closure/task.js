cc602a9431f68ae1bb0f3a606898b21874b6c467
vapanov <valentin.panov@me.com>
2020-10-21 23:22:44 +0300

    3.1 1jg 2jg/r

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -17,3 +17,33 @@ function validateCount(value) {
 
 // TASK 2
 
+class Triangle {
+   constructor(a,b,c) {
+
+      if ((a + b) < c || (b + c) < a || (c + a) < b) throw new Error ("Треугольник с такими сторонами не существует");
+
+      this.a = a;
+      this.b = b;
+      this.c = c;
+   }
+
+   getPerimeter(){
+      return (this.a + this.b + this.c);
+   }
+
+   getArea(){
+      debugger;
+      let p = this.getPerimeter() / 2;
+      return Number(Math.sqrt(p * (p - this.a)* (p - this.b)* (p - this.c)).toFixed(3));
+   }
+}
+
+function getTriangle(a,b,c) {
+   debugger;
+   try {
+      return new Triangle(a,b,c);
+   } catch {
+      throw ({getArea: "Ошибка! Треугольник не существует", getPerimeter: "Ошибка! Треугольник не существует"}); // пока не понимаю, как пробросить объект с методами, которые возвращают такие строки
+   }
+   
+}
