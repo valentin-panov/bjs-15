38751fe5769faddca195ef0cc5a7ceac7b2469cf
vapanov <valentin.panov@me.com>
2020-10-24 22:24:30 +0300

    3.2-1 2nd green

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -3,28 +3,13 @@ console.clear();
 
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 
-
 function getNames() {
    return weapons.map(function getName (arg) {
-      debugger;
             return arg.name;
          }
       );
 }
 
-
 function getCountReliableWeapons (askedDurability) {
-
-   debugger;
-
-   console.log(weapons.map(function moreDurable (arg, askedDurability) {
-      if (arg.durability > askedDurability) {
-         return arg;
-      } else {
-         return null;
-      }
-   }).length);
-   
-   return weapons.map(function moreDurable (arg, askedDurability)).length;
-
-}
+    return weapons.filter(weapon => ((weapon.durability > askedDurability) === true)).length;
+ }
