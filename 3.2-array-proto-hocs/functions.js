3917f26f7583fa6f48f9a7a020644e163a8ac69f
vapanov <valentin.panov@me.com>
2020-10-25 11:13:11 +0300

    3.2-1 3d stated

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -11,5 +11,9 @@ function getNames() {
 }
 
 function getCountReliableWeapons (askedDurability) {
-    return weapons.filter(weapon => ((weapon.durability > askedDurability) === true)).length;
+    return weapons.filter(weapon => ((weapon.durability > askedDurability) === true)).length;  // не понимаю, откуда у нас взялся weapon, и что это такое
+ }
+
+ function hasReliableWeapons (askedDurability) {
+   return weapons.find(weapon => ((weapon.durability > askedDurability) === true));
  }
