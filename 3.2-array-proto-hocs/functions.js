6772283a3d6f13fb3181a1ca79fb70b8460cece3
vapanov <valentin.panov@me.com>
2020-10-25 18:56:02 +0300

    3.2-1 j-g

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -11,9 +11,19 @@ function getNames() {
 }
 
 function getCountReliableWeapons (askedDurability) {
-    return weapons.filter(weapon => ((weapon.durability > askedDurability) === true)).length;  // не понимаю, откуда у нас взялся weapon, и что это такое
+    return weapons.filter(weapon => ((weapon.durability > askedDurability) === true)).length;  // не понимаю, откуда у нас взялся weapon, и что это такое в рамках стрелочной функции
  }
 
  function hasReliableWeapons (askedDurability) {
-   return weapons.find(weapon => ((weapon.durability > askedDurability) === true));
+   return (weapons.some(weapon => weapon.durability > askedDurability));
+ }
+
+ function getReliableWeaponsNames (askedDurability) {
+    return (weapons.filter(weapon => ((weapon.durability > askedDurability) === true)).map(weapon => weapon.name));
+ }
+
+ function getTotalDamage () {
+   return  weapons.reduce((totalDamage, weapon, index) => {
+      return totalDamage + weapon.attack;
+   }, 0);
  }
