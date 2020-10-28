67118fe8d720734681c6f7d37ebacd3c65963205
vapanov <valentin.panov@me.com>
2020-10-28 21:14:24 +0300

    3.2.1 jg, 3.2 broke my mind

--- 3.2-array-proto-hocs/functions.js
+++ 3.2-array-proto-hocs/functions.js
@@ -7,33 +7,30 @@ console.clear();
 const weapons = [new Knife(), new Staff(), new Axe(), new StormStaff(), new LongBow(), new Bow()];
 
 function getNames() {
-   return weapons.map(function getName (arg) { // нужно ли передавать в эту функцию объект? или работать с тем, что есть?
-            return arg.name;
-         }
-      );
+   return weapons.map(weapon => weapon.name);
+}
+
+function hasReliableWeapons (askedDurability) {
+  return (weapons.some(weapon => weapon.durability > askedDurability));
 }
 
 function getCountReliableWeapons (askedDurability) {
-    return weapons.filter(weapon => ((weapon.durability > askedDurability) === true)).length;  // не понимаю, откуда у нас взялся weapon, и что это такое в рамках стрелочной функции
- }
+  return weapons.filter(weapon => ((weapon.durability > askedDurability) === true)).length;
+}
 
- function hasReliableWeapons (askedDurability) {
-   return (weapons.some(weapon => weapon.durability > askedDurability));
- }
+function getReliableWeaponsNames (askedDurability) {
+  return (weapons.filter(weapon => ((weapon.durability > askedDurability) === true)).map(weapon => weapon.name));
+}
 
- function getReliableWeaponsNames (askedDurability) {
-    return (weapons.filter(weapon => ((weapon.durability > askedDurability) === true)).map(weapon => weapon.name));
- }
+function getTotalDamage () {
+  return  weapons.reduce((totalDamage, weapon, index) => (totalDamage + weapon.attack), 0);
+  // написал и забыл, что это за 0 в конце 8( - начальный индекс?
+  // totalDamage попадает в замыкание reduce?
+}
 
- function getTotalDamage () {
-   return  weapons.reduce((totalDamage, weapon, index) => {
-      return totalDamage + weapon.attack;
-   }, 0);
- }
+// TASK 2
 
- // TASK 2
-
- function sleep(milliseconds) {
+function sleep(milliseconds) {
   let e = new Date().getTime() + milliseconds;
   while (new Date().getTime() <= e) {}
 }
@@ -46,14 +43,24 @@ function sum(...args) {
 }
 
 function compareArrays(arr1, arr2) {
-   if (!arr1 || !arr2 || (arr1.length != arr2.length)) {return false;} // ускоряем обработку, отлавливаем явные несоответсвия
-   if (arr1.every((value, index) => value === arr2[index])) { // сравниваем значения по индексу
-      return true; // всё сошлось
-   }
-   return false; // не сошлось
+  if (!arr1 || !arr2 || (arr1.length != arr2.length)) {return false;} // ускоряем обработку, отлавливаем явные несоответсвия и неверный вход
+  if (arr1.every((value, index) => value === arr2[index])) { // сравниваем значения по индексу
+    return true; // всё сошлось
+  }
+  return false; // не сошлось
 }
 
-function memorize(fn, limit) { // fn - функция, которая производит вычисления, limit - ограничение по количеству результатов
-   let memory = { args, result };
-   return fn;
+function memorize(fn, limit, ...theArgs) { // fn - функция, которая производит вычисления, limit - ограничение по количеству запоминаемых результатов, theArgs - аргументы для вызываемой функции
+  const memory = []; // создаём замкнутую память {args, result}
+
+
+  function fn () {
+    if (memory.find()) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  return fn;
 }
