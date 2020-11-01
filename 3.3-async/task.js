29a68f9e1bca480b5c0ce30a531e2cb7c51a0981
vapanov <valentin.panov@me.com>
2020-11-01 21:31:24 +0300

    3.3 up to start green?

--- 3.3-async/task.js
+++ 3.3-async/task.js
@@ -5,26 +5,26 @@ class AlarmClock {
       this.alarmCollection = []; // коллекция будильников
       this.timerId; // айди таймера
    }
-   addClock (time, fn, id){  // метод принимает время (формат hh:mm) коллбэк и айди
+
+   addClock (time, fn, id){  
+// добавляет новый звонок в коллекцию существующих
+// метод принимает время (формат hh:mm) коллбэк и айди
       if (!id) {throw new Error('error text');};  // если id не задан, пробрасываем ошибку
       if (this.alarmCollection.some(timer => timer.id == id)) {  // если id не уникален, пробрасываем console.error()
-         //debugger; не разобрался, почему это пробрасывает ошибку, но не зеленит жасмин
-         throw console.error(`Error creating new alarm. Alarm ID \"${id}\" isn't unique.`);
-         // как завершить метод?
+         throw console.error(`Error creating new alarm. Alarm ID \"${id}\" isn't unique.`); // не понимаю, метод работает как требуется, а жасмин красный (
       }
       this.alarmCollection.push({id: id, time: time, callback: fn}); //Перед завершением метода добавьте в массив звонков объект со свойствами id, time, callback.
+   }
 
-   }
    removeClock (id) {
-      console.log(this.alarmCollection.find(alarm => (alarm.id == id)));
-//      const removedAlarm = this.alarmCollection.filter(alarm => (alarm.id == id));
-//      removedAlarm.splice(0, 1);
-      console.log(this.alarmCollection);
-//      console.log(removedAlarm);
-//      console.log(removedAlarm.length);
-      return removedAlarm.length;
+// удаляет определённый звонок
+// Верните логическое значение об успешности/провале удаления объекта звонка из общего массива
+      return !!(this.alarmCollection.splice(this.alarmCollection.findIndex(alarm => (alarm.id == id)), 1).length);
    }
+
    getCurrentFormattedTime () {
+// возвращает текущее время в строковом формате HH:MM.
+      return (`${new Date().getHours()}:${new Date().getMinutes()}`);
 
    }
    start () {
